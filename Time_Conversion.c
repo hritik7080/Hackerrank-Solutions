d3cc08f3a1043776c8dc8456c089615f6ae018c4
hritik7080 <41755284+hritik7080@users.noreply.github.com>
2018-12-10 21:55:02 +0530

    Create Time_Conversion.c

---
+++ Time_Conversion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int a,b,c;
+    char s[2];
+    scanf("%d:%d:%d%s",&a,&b,&c,s);
+    if(strcmp(s,"PM")==0 && a!=12)
+    a+=12;
+    if(strcmp(s,"AM")==0 && a==12)
+    a=0;
+    printf("%02d:%02d:%02d",a,b,c);
+    return 0;
+}
+/*If you have any suggestion or any issue feel free to post in issue tab*/
